d14fe90ab655aa752053a3fcf27d330720c40d2c
Jose Diaz <jose@aircury.com>
2026-09-13 22:44:23 +0100

    test(app): name the image stub before exporting it as the default

--- projects/marketing/test/image-stub.ts
+++ projects/marketing/test/image-stub.ts
@@ -1,1 +1,3 @@
-export default { src: "/test-image.png", height: 1, width: 1, blurDataURL: "", blurWidth: 0, blurHeight: 0 };
+const imageStub = { src: "/test-image.png", height: 1, width: 1, blurDataURL: "", blurWidth: 0, blurHeight: 0 };
+
+export default imageStub;
